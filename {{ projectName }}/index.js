0b274e15ef88fdf07f4a9404af2d1191582bdcd0
MikasaAckerman <haymai@163.com>
2020-12-23 20:21:19 +0800

    Update index.js

--- {{ projectName }}/index.js
+++ {{ projectName }}/index.js
@@ -25,24 +25,25 @@ module.exports.initializer = async (context, callback) => {
   callback(null, '');
 };
 
-const screenshot = async (url) => {
+const convert2pdf = async (url) => {
   const page = await browser.newPage();
-  const outputFile = '/tmp/screenshot.png';
+  const outputFile = '/tmp/temp.pdf';
   await page.goto(url);
-  await page.screenshot({
-    path: outputFile,
-    fullPage: true
+  await page.evaluate(async () => {
+    //todo 执行页面优化js
+    console.log("hello world");
   });
-  console.log(`The screenshot for url ${url} saved to ${outputFile}`);
+  await page.pdf({path: outputFile, format: 'A4'});
+  console.log(`The convert2pdf for url ${url} saved to ${outputFile}`);
   page.close();
   return outputFile;
 };
 
 module.exports.handler = async (req, resp, context) => {
-  const pageUrl = req.queries["url"] || 'https://www.aliyun.com';
+  const pageUrl = req.queries["url"] || 'https://www.fxbaogao.com';
   console.log(`page url: ${pageUrl}`);
-  const file = await screenshot(pageUrl);
-  const filename = "screenshot.png";
+  const file = await convert2pdf(pageUrl);
+  const filename = "temp.pdf";
   resp.setHeader('Context-Type', 'application/octet-stream');
   resp.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
   resp.send(fs.createReadStream(file));
